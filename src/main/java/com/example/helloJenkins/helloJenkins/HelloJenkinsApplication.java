925402f919e808df2a42f8d95871f6f0db4fdd86
=parksungsu <pss89513@gmail.com>
2017-12-09 00:53:35 +0900

    jenkins test

---
+++ src/main/java/com/example/helloJenkins/helloJenkins/HelloJenkinsApplication.java
@@ -0,0 +1,19 @@
+package com.example.helloJenkins.helloJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloJenkinsApplication {
+
+	@GetMapping(value = "/")
+	public String helloJenkins() {
+		return "helloJenkins";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsApplication.class, args);
+	}
+}
