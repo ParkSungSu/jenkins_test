002f44cb6c3951a4fc34b6bbef852501d81770dd
=parksungsu <pss89513@gmail.com>
2018-05-10 11:57:32 +0900

    젠킨스테스트입니다.

--- src/main/java/com/example/helloJenkins/helloJenkins/HelloJenkinsApplication.java
+++ src/main/java/com/example/helloJenkins/helloJenkins/HelloJenkinsApplication.java
@@ -11,7 +11,7 @@ public class HelloJenkinsApplication {
 
 	@GetMapping(value = "/")
 	public String helloJenkins() {
-		return "helloJenkins999";
+		return "helloJenkins1119";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJenkinsApplication.class, args);
