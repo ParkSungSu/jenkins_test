8bf5dad9205e760a4b4b2e1d13e9a5ea66a6bca6
=parksungsu <pss89513@gmail.com>
2018-05-10 17:46:35 +0900

    젠킨스테스트2

--- src/main/java/com/example/helloJenkins/helloJenkins/HelloJenkinsApplication.java
+++ src/main/java/com/example/helloJenkins/helloJenkins/HelloJenkinsApplication.java
@@ -11,7 +11,7 @@ public class HelloJenkinsApplication {
 
 	@GetMapping(value = "/")
 	public String helloJenkins() {
-		return "helloJenkins1119";
+		return "helloJenkins!!!!!!!!!!";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJenkinsApplication.class, args);
